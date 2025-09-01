d17244be3e89d8eee239dd89dc312d0ae56e1cd2
VictorOduorke <victoroduorxh2@gmail.com>
2025-09-01 17:31:53 +0300

    added api url corrected api url

--- backend/study_plan.py
+++ backend/study_plan.py
@@ -20,7 +20,7 @@ app.secret_key = os.getenv("SECRET_KEY")
 # ✅ Enable CORS for your frontend
 CORS(
     app,
-    origins=["http://127.0.0.1:5501"],
+    origins=["http://127.0.0.1:5500"],
     supports_credentials=True,
     methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["Content-Type"],
